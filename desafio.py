570072983f7f4fff75a191a8f867dc240588e46a
gustavo-ross <150253416+gustavo-ross@users.noreply.github.com>
2026-03-31 20:41:51 -0300

    Update_tentativas.py

--- desafio.py
+++ desafio.py
@@ -8,7 +8,7 @@ Print("JOGO DE ADIVINHAÇÃO")
 print("Tente adivinhar o número secreto entre 1 e 100 para vencer!")
 
 while True:
-    if tentativa == 8:
+    if tentativa == 7:
         print(f"Você perdeu. O número máximo de tentativas foi atingido. O número secreto era {num}.")
         break
 
